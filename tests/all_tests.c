638a52dacc75f9a26a84ad85aa80b4a94ee77482
root <root@Ubuntu.myguest.virtualbox.org>
2024-03-22 15:42:15 -0700

    trying to get tests for measurement to work

--- tests/all_tests.c
+++ tests/all_tests.c
@@ -8,6 +8,7 @@ int main(void) {
     // Call the test runner functions for each test module
     test_qubit_run_tests();
     test_xgate_run_tests();
+    test_measurement_run_tests(); // Call the measurement test suite runner
 
     return UNITY_END();
 }
